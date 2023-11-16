ee8a284b0155044d2c439f82d74c69013526f87f
Ivan Dibrov <evrjmet@gmail.com>
2023-11-16 21:49:25 +0300

    Добавьте файлы проекта.

---
+++ Perimetr/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.Write("Введите периметр треугольника: ");
+        double perimeter = Convert.ToDouble(Console.ReadLine());
+
+        double side = perimeter / 3;
+        double semiPerimeter = perimeter / 2;
+
+        double area = Math.Sqrt(semiPerimeter * Math.Pow(semiPerimeter - side, 3));
+
+        Console.WriteLine("\nСторона\t\tПлощадь");
+        Console.WriteLine($"{side:F2}\t\t{area:F2}");
+        Console.ReadLine();
+    }
+}
